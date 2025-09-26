6bae8cb9a895ce6d5be0e9bf6af0119f29d8028d
Vishal Toora <106994336+vtooraipcium@users.noreply.github.com>
2025-09-26 12:45:51 -0700

    fix: simplify webpack config to match MyPersn pattern - remove unnecessary externals

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,7 +1,7 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 const path = require('node:path');
 
-// Nx plugins for webpack - MyPersn pattern with shared library fix
+// Nx plugins for webpack - MyPersn pattern (simplified)
 module.exports = composePlugins(withNx(), (config) => {
   // Note: This was added by an Nx migration. Webpack builds are required to have a corresponding Webpack config file.
   // See: https://nx.dev/recipes/webpack/webpack-config-setup
@@ -20,17 +20,6 @@ module.exports = composePlugins(withNx(), (config) => {
   // Ensure source maps are included
   config.module = config.module || {};
   config.module.rules = config.module.rules || [];
-
-  // CRITICAL: Externalize shared libraries for Docker deployment
-  // These will be available in node_modules at runtime
-  config.externals = {
-    '@prisma/client': 'commonjs @prisma/client',
-    '.prisma/client': 'commonjs .prisma/client',
-    '@nestjs/websockets/socket-module': 'commonjs @nestjs/websockets/socket-module',
-    '@nestjs/microservices/microservices-module': 'commonjs @nestjs/microservices/microservices-module',
-    '@nestjs/microservices': 'commonjs @nestjs/microservices',
-    '@gt-automotive/shared-dto': 'commonjs @gt-automotive/shared-dto',
-  };
 
   return config;
 });
